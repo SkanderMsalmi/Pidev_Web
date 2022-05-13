a60462d28e1a25cf31cf4f13ffc280fbf92bc67d
SkanderMsalmi <msalmi.skander@esprit.tn>
2022-05-13 04:23:14 +0100

    pass to admin in login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -74,6 +74,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
 
+
         return $user;
     }
 
@@ -84,11 +85,14 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
 
 
+
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
+
+
             return new RedirectResponse($this->urlGenerator->generate('acceuil'));
     }
 
